0128fd2a114d20f51b0a90f1a3ebfd9743f98489
root <root@localhost.localdomain>
2019-04-14 16:36:53 +0800

    Commit

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ import java.io.*;
 public class Main {
 	public static void public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		LinkedList tickets = new LinkedList();
+		Queue order = new Queue();
 	}
 }
